79b89705df20b866d05e48c860d6a9c7917c78bf
tobiasrausch <rauschtobi@gmail.com>
2017-02-24 12:46:12 +0100

    removed atac.sh

--- atac.sh
+++ atac.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-if [ $# -ne 4 ]
-then
-    echo "**********************************************************************"
-    echo "ATAC-Seq analysis pipeline."
-    echo "This program comes with ABSOLUTELY NO WARRANTY."
-    echo ""
-    echo "Contact: Tobias Rausch (rausch@embl.de)"
-    echo "**********************************************************************"
-    echo ""
-    echo "Usage: $0 <read1.fq.gz> <read2.fq.gz> <genome.fa> <output prefix>"
-    echo ""
-    exit -1
-fi
+
 
 SCRIPT=$(readlink -f "$0")
 BASEDIR=$(dirname "$SCRIPT")
